81ebd90b9b8f0cc14c55b5bc9d8b2e5a69657b94
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-01-04 14:54:52 -0300

    Exercícios intensos resoluções de python atualizado.

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+r = n**(1/2)
+print(f'O dobro de {n} vale {n*2}')
+print(f'O triplo de {n} vale {n*3}.')
+print(f'A raiz quadrada de {n} é {r:.2f}!')
---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+s = n1+n2
+m = s/2
+print(f'A média entre {n1:.1f} e {n2:.1f} é {m/2:.1f}')
