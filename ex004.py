13238df208d0742671ef0f90bd688f120e2a5617
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-01-04 11:11:33 -0300

    Exercícios iniciais para treino.

---
+++ ex004.py
@@ -0,0 +1,8 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print ('Só tem espaços? ', a.isspace())
+print ('É um número? ', a.isnumeric())
+print ('É alfabético? ', a.isnumeric())
+print ('É alfanumérico? ', a.isalnum())
+print ('Está em maiúscula? ', a.isupper())
+print('Esrá capitalizada?', a.istitle())
