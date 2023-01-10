a4a6ca088e07b019e07ea524308df6dbc9c98fdc
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-01-10 08:17:16 -0300

    Ótimos exercícios em Python para iniciantes.

---
+++ ex015a-aluguel-de-carro.py
@@ -0,0 +1,5 @@
+#O dia vale R$ 0,15 #
+dias = int(input('Quantos dias alugados? '))
+km = float(input('Quantos km rodados? '))
+pago = (dias * 60) + (km * 0.15)
+print(f'O total a pagar é de R$ {pago:.2f}')
---
+++ ex016-quebrando-um-numero.py
@@ -0,0 +1,7 @@
+'''from math import trunc
+num = float(input('Digite um valor: '))
+print(f'O  valor digitado foi {num} e sua porção inteira é {trunc(num)} ')
+'''
+
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {int(num)}')
